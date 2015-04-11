a518faeb81667d0795757ce7379fc7d16082fef9
Takuya OHASHI <tky.ohashi@gmail.com>
2015-04-11 17:47:27 +0900

    add macro/types headers

---
+++ include/tos_macro.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2015, TAKUYA Ohashi. All rights reserved.
+ */
+#ifndef TOS_MACRO_H_INCLUDED
+#define TOS_MACRO_H_INCLUDED
+
+#define TOS_MIN(a,b) ((a)<(b)?(a):(b))
+#define TOS_MAX(a,b) ((a)>(b)?(a):(b))
+
+#endif /* TOS_MACRO_H_INCLUDED */
---
+++ include/tos_types.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2015, TAKUYA Ohashi. All rights reserved.
+ */
+#ifndef TOS_TYPES_H_INCLUDED
+#define TOS_TYPES_H_INCLUDED
+
+#include <stdint.h>
+
+#endif /* TOS_TYPES_H_INCLUDED */
