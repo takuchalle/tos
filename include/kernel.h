5df714eb04f5d2cff80723ac7c7c526d932293f5
Takuya OHASHI <tky.ohashi@gmail.com>
2015-05-13 23:48:01 +0900

    add state

--- include/kernel.h
+++ include/kernel.h
@@ -4,9 +4,19 @@
 #ifndef KERNEL_H_INCLUDED
 #define KERNEL_H_INCLUDED
 
+#include "tos_types.h"
+
+typedef enum _kernel_STATE {
+	_kernel_STATE_ACTIVE = 0,
+	_kernel_STATE_SLEEP,
+	_kernel_STATE_IDLE,
+	_kernel_STATE_FREE,
+	_kernel_STATE_READY,
+	_kernel_STATE_NUM,
+} _kernel_STATE_t;
+
 typedef struct _kernel_TCB {
-      
-	
-} _kernel_TCB_t
+	_kernel_STATE_t state;
+} _kernel_TCB_t;
 
 #endif /* KERNEL_H_INCLUDED */
