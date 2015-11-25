a5ff40c5ecee02651b01d3f607402c040245aa1d
Takuya OHASHI <tky.ohashi@gmail.com>
2015-11-26 01:08:04 +0900

    use "arm-none-eabi-gcc"
     - install from https://launchpad.net/gcc-arm-embedded/+download

--- test/config/tools.mk
+++ test/config/tools.mk
@@ -2,7 +2,7 @@
 # Copyright (c) 2015, TAKUYA Ohashi. All rights reserved.
 #
 
-TOOL_PREFIX=/usr/bin/arm-linux-gnueabi-
+TOOL_PREFIX=arm-none-eabi-
 CC = $(TOOL_PREFIX)gcc
 LD = $(TOOL_PREFIX)ld
 OBJCOPY = $(TOOL_PREFIX)objcopy
